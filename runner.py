971ebd33998ef139e9215bff9d4d80e6d7741ce1
ThomasNagel <tmj.nagel@gmail.com>
2024-05-04 23:06:30 +0200

    feat:"metrics csv" get all metrics based on settings in config.json and write them to a csv file at the end

--- runner.py
+++ runner.py
@@ -1,7 +1,6 @@
 import psutil
-import sys
-import datetime
 import time
+import csv
 
 class Runner():
     #----------------------------------------------------------------
@@ -10,17 +9,21 @@ class Runner():
     def __init__(self, config: dict):
         self.config: dict = config
         self.target: psutil.Popen = None
+        self.metrics: list[dict] = list()
+
+        print(f'Settings: {config}')
 
     def run(self):
         start_wall_time = time.time()
 
-        with open(self.config['target']['logs'], '+a') as f:
-            f.write(f'Time: {datetime.datetime.now()}\tConfig settings:\n{self.config}\nTarget output:\n')
+        with open(self.config['target']['log'], '+w') as f:
             self.target = psutil.Popen(self.config['target']['args'], stdout=f)
         
         while self.target.poll() == None:
-            self.update_metrics()
+            self.metrics.append(self.target.as_dict(attrs=self.config['runner']['metrics']['attrs']))
             time.sleep(config['runner']['tick_delay'])
+
+        self.write_metrics()
 
         print(f'Target return status: {self.target.wait()}')
         print(f"Rough wall time: {time.time() - start_wall_time}")        
@@ -28,10 +31,27 @@ class Runner():
     #----------------------------------------------------------------
     # PRIVATE METHODS
     #----------------------------------------------------------------
-    def update_metrics(self):
-        with self.target.oneshot():
-            print(self.target.cpu_percent())
-            print(self.target.memory_full_info())
+    def write_metrics(self):
+        flat_metrics: list[dict] = [self.flatten_metric(metric) for metric in self.metrics]
+
+        with open(self.config['runner']['metrics']['out'], '+w') as f:
+            dict_writer = csv.DictWriter(f, flat_metrics[0].keys())
+            dict_writer.writeheader()
+            dict_writer.writerows(flat_metrics)
+    
+    def flatten_metric(self, metric: dict) -> dict:
+        flat = dict()
+
+        for key, metric_val in metric.items():
+            if isinstance(metric_val, int | float | str | bool):
+                flat[key] = metric_val
+            else:  # Should be a named tuple according to the psutils docs
+                for name, tuple_val in metric_val._asdict().items():
+                    flat[f'{key}-{name}'] = tuple_val
+
+        return flat
+
+
 
 #----------------------------------------------------------------
 # Testing
